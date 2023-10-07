ea0fe222a9768392ad9685d83f2da3096af582c0
robertdilworth <robertdilworth@gmail.com>
2023-10-07 09:13:43 -0400

    adding mui.com for frontend

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4caf50',
+    },
+    secondary: {
+      main: '#ff9100',
+    },
+  },
+});
+
+export default theme;
